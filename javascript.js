a54b91d5dd336fe4c7926ceeda757766949a2f71
megafayray <fay.brotherston@gmail.com>
2024-07-10 16:07:28 -0400

    Change alerts to DOM textContent methods

--- javascript.js
+++ javascript.js
@@ -8,20 +8,20 @@ const gameOver = document.querySelector("#gameOver");
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
-        alert("It's a draw! Your score:" + " " + humanScore + " & " + "Computer score:" + " " + computerScore);
+        roundResults.textContent = "It's a draw!";
     }
     else if(humanChoice === "rock" && computerChoice === "scissors" ||
         humanChoice === "paper" && computerChoice === "rock" ||
         humanChoice === "scissors" && computerChoice === "paper") {
         humanScore++;
-        alert("You win: " + humanChoice + " beats " + computerChoice + "! " + "Your score:" + " " + humanScore + " & " + "Computer score:" + " " + computerScore);
+        roundResults.textContent = "You won this round: " + humanChoice + " beats " + computerChoice;
         }
 
     else if(humanChoice === "rock" && computerChoice === "paper" ||
         humanChoice === "paper" && computerChoice === "scissors" ||
         humanChoice === "scissors" && computerChoice === "rock"){
         computerScore++;
-        alert("You lose: " + computerChoice + " beats " + humanChoice + "! " + "Your score:" + " " + humanScore + " & " + "Computer score:" + " " + computerScore);
+        roundResults.textContent = "You lost this round: " + computerChoice + " beats " + humanChoice;
         }
     else{
         alert("Somethings not working here - please try again");
